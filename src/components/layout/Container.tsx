c06bb18fe478477dea021e71545d09f451e1aca3
zackkh <44476801+zackkh@users.noreply.github.com>
2024-03-20 18:46:17 +0000

    Add hideSidebar prop

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,17 +1,27 @@
+import clsx from "clsx";
 import React from "react";
 import Layout from "./Layout";
 import LeftSidebar from "./LeftSidebar";
 
-const LayoutContainer: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+const LayoutContainer: React.FC<{
+  children: React.ReactNode;
+  hideSidebar?: boolean;
+}> = ({ children, hideSidebar = false }) => {
   return (
     <Layout>
       <div className="mx-auto max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
-        <div className="hidden lg:col-span-3 lg:block xl:col-span-2">
-          <LeftSidebar />
-        </div>
-        <main className="lg:col-span-9 xl:col-span-10">{children}</main>
+        {!hideSidebar && (
+          <div className="hidden lg:col-span-3 lg:block xl:col-span-2">
+            <LeftSidebar />
+          </div>
+        )}
+        <main
+          className={clsx(
+            hideSidebar ? "lg:col-span-12" : "lg:col-span-9 xl:col-span-10",
+          )}
+        >
+          {children}
+        </main>
       </div>
     </Layout>
   );
